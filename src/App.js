3879f95dd10b9a7b920e71f7c8ab7f39bd85bf00
rockman4417 <67634648+rockman4417@users.noreply.github.com>
2021-01-18 10:20:57 -0600

    created todo functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
+import React, {Component} from 'react'
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      isClicked: true,
+      inputValue: "",
+      listOfTodos: []
+    }
+  }
+
+  handleClick = () => {
+    this.state.isClicked ? 
+    this.setState({isClicked: false}) : 
+    this.setState({isClicked: false})
+  }
+
+  handleChange = (event) => {
+    this.setState({inputValue: event.target.value})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({listOfTodos: [...this.state.listOfTodos, this.state.inputValue]})
+    this.setState({inputValue: ""})
+  }
+
+render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          DUM CANT
-        </p>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.inputValue} onChange={this.handleChange}></input>
+          <button type="submit">Submit</button>
+        </form>
+        <ol>{this.state.listOfTodos.map((todo, index) => {
+          return <li key={index}>{todo}</li>
+        })}</ol>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +53,9 @@ function App() {
       </header>
     </div>
   );
+}
+
+  
 }
 
 export default App;
